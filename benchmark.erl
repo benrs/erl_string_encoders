54eb8f32825d0cfc8d4abc69a21e8074d56ccee7
Benjamin Barault <b.barault@adgear.com>
2019-08-16 10:42:39 -0400

    Add more options for benchmarking

--- benchmark.erl
+++ benchmark.erl
@@ -1,58 +1,73 @@
 -module(benchmark).
 
 -export([
-    run_base64_encode/0,
-    run_base64_decode/0,
-    run_tolower/0,
-    random_string/0
+    run_base64_encode/2,
+    run_base64_decode/2,
+    run_tolower/2
 ]).
 
+exhaust_mail_box(0) ->
+    ok;
+exhaust_mail_box(SpawnAmount) ->
+    receive
+        Result when is_binary(Result) -> exhaust_mail_box(SpawnAmount - 1)
+    end.
+
 -define(spawn_and_get_response(Fun),
-    fun() ->
-        MyPid = self(),
-        spawn(
-            fun() ->
-                Result = Fun,
-                MyPid ! Result
+    fun (Mode, SpawnAmount) ->
+        fun() ->
+            case Mode of
+                parallel ->
+                    MyPid = self(),
+                    [ spawn(
+                        fun() ->
+                            Result = Fun,
+                            MyPid ! Result
+                        end
+                    ) || _A <- lists:seq(1, SpawnAmount)],
+                    exhaust_mail_box(SpawnAmount);
+                serial ->
+                    Fun
             end
-        ),
-        receive
-            Result when is_binary(Result) -> Result
-        after 100
-            -> erlang:display("Did not receive result")
         end
     end
 ).
 
-run_base64_encode() ->
+run_base64_encode(Length, {TestMode, Amount}) ->
     rand:seed(exs64, os:timestamp()),
-    DataSetC = eministat:s("CBase64Encode", ?spawn_and_get_response(erl_string_encoders:base64_encode(random_string())), 200),
+    DataSetC = eministat:s("CBase64Encode", ?spawn_and_get_response(erl_string_encoders:base64_encode(random_string(Length)))(TestMode, Amount), 200),
     timer:sleep(5000),
-    DataSetErl = eministat:s("ErlangBase64Encode", ?spawn_and_get_response(base64:encode(random_string())), 200),
-    eministat:x(95.0, DataSetC, DataSetErl).
+    DataSetErl = eministat:s("ErlangBase64Encode", ?spawn_and_get_response(base64:encode(random_string(Length)))(TestMode, Amount), 200),
+    eministat:x(95.0, DataSetC, DataSetErl);
+run_base64_encode(Length, TestMode) ->
+    run_base64_encode(Length, {TestMode, 1}).
 
-run_base64_decode() ->
+run_base64_decode(Length, {TestMode, Amount}) ->
     rand:seed(exs64, os:timestamp()),
-    DataSetC = eministat:s("CBase64Decode", ?spawn_and_get_response(erl_string_encoders:base64_decode(base64:encode(random_string()))), 200),
+    DataSetC = eministat:s("CBase64Decode", ?spawn_and_get_response(erl_string_encoders:base64_decode(base64:encode(random_string(Length))))(TestMode, Amount), 200),
     timer:sleep(5000),
-    DataSetErl = eministat:s("ErlangBase64Decode", ?spawn_and_get_response(base64:decode(base64:encode(random_string()))), 200),
-    eministat:x(95.0, DataSetC, DataSetErl).
+    DataSetErl = eministat:s("ErlangBase64Decode", ?spawn_and_get_response(base64:decode(base64:encode(random_string(Length))))(TestMode, Amount), 200),
+    eministat:x(95.0, DataSetC, DataSetErl);
+run_base64_decode(Length, TestMode) ->
+    run_base64_decode(Length, {TestMode, 1}).
 
-run_tolower() ->
+run_tolower(Length, {TestMode, Amount}) ->
     rand:seed(exs64, os:timestamp()),
-    DataSetC = eministat:s("CBase64Lower", ?spawn_and_get_response(erl_string_encoders:to_lower(random_string())), 200),
+    DataSetC = eministat:s("CBase64Lower", ?spawn_and_get_response(erl_string_encoders:to_lower(random_string_upper(Length)))(TestMode, Amount), 200),
     timer:sleep(5000),
-    DataSetErl = eministat:s("ErlangBase64Lower", ?spawn_and_get_response(string:lowercase(random_string_upper())), 200),
-    eministat:x(95.0, DataSetC, DataSetErl).
+    DataSetErl = eministat:s("ErlangBase64Lower", ?spawn_and_get_response(string:lowercase(random_string_upper(Length)))(TestMode, Amount), 200),
+    eministat:x(95.0, DataSetC, DataSetErl);
+run_tolower(Length, TestMode) ->
+    run_tolower(Length, {TestMode, 1}).
 
-random_string() ->
-    binary:list_to_bin(get_random_string(30, [
+random_string(Length) ->
+    binary:list_to_bin(get_random_string(Length, [
         "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
         "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
     ])).
 
-random_string_upper() ->
-    binary:list_to_bin(get_random_string(30, [
+random_string_upper(Length) ->
+    binary:list_to_bin(get_random_string(Length, [
         "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
     ])).
 
